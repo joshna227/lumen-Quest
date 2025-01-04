5b31dd2e1b9a27453c2f93c2319fd6c946a9e7a7
joshna227 <134677454+joshna227@users.noreply.github.com>
2025-01-04 15:04:01 +0530

    Create auth.middleware.js

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+    const token = req.headers["authorization"];
+    if (!token) {
+        return res.status(403).json({ message: "No token provided!" });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+        req.userRole = decoded.role;
+        next();
+    });
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (req.userRole !== "Admin") {
+        return res.status(403).json({ message: "Require Admin Role!" });
+    }
+    next();
+};
+
+exports.isManager = (req, res, next) => {
+    if (req.userRole !== "Manager") {
+        return res.status(403).json({ message: "Require Manager Role!" });
+    }
+    next();
+};
